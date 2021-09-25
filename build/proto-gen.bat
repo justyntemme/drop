4d9120db923da9b1f01ed650d3770e76411f2bc3
justyn <justyntemme@gmail.com>
2021-09-25 13:04:24 -0500

    Change missing reference and remove gopkg files

--- build/proto-gen.bat
+++ build/proto-gen.bat
@@ -1,2 +1,2 @@
-protoc.exe -I $env:GOPATH\src --go_out=$env:GOPATH\src $env:GOPATH\src\bitbucket-repository-management-service\internal\proto-files\domain\repository.proto
-protoc.exe -I $env:GOPATH\src --go_out=plugins=grpc:$env:GOPATH\src $env:GOPATH\src\bitbucket-repository-management-service\internal\proto-files\service\repository-service.proto
+protoc.exe -I $env:GOPATH\src --go_out=$env:GOPATH\src $env:GOPATH\src\gitlab.com/nextwavedevs/drop-service\internal\proto-files\domain\repository.proto
+protoc.exe -I $env:GOPATH\src --go_out=plugins=grpc:$env:GOPATH\src $env:GOPATH\src\gitlab.com/nextwavedevs/drop-service\internal\proto-files\service\repository-service.proto
