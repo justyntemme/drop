d2a91bc43dbaa5461fe995e5c576b427f666d4b9
justyn <justyntemme@gmail.com>
2021-09-25 13:32:35 -0500

    pushing latest to make repo have all required internal deps

--- build/proto-gen.sh
+++ build/proto-gen.sh
@@ -1,2 +1,2 @@
-protoc -I $GOPATH/src --go_out=$GOPATH/src $GOPATH/src/gitlab.com/nextwavedevs/drop/internal/proto-files/domain/repository.proto
-protoc -I $GOPATH/src --go_out=plugins=grpc:$GOPATH/src $GOPATH/src/gitlab.com/nextwavedevs/drop/internal/proto-files/service/repository-service.proto
+protoc -I=/mnt/d/Projects/drop --go_out=. /mnt/d/Projects/drop/internal/proto-files/domain/listing.proto
+protoc -I=/mnt/d/Projects/drop --go_out=. /mnt/d/Projects/drop/internal/proto-files/service/listingInfoService.proto
